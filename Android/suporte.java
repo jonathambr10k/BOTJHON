bbfa071a69afc57b297b4e40dd6c79b0e105301c
jonathambr10k <79543009+jonathambr10k@users.noreply.github.com>
2021-03-01 15:11:33 -0300

    Delete Android directory

--- Android/suporte.java
+++
@@ -1,9 +0,0 @@
-/*
- * Classname             (darkbot3.0)
- *
- * Version information   (3.0)
- *
- * Date                  (06/12,15:35)
- *
- * author                (Dark YT)
- * Copyright notice      (bot com suporte a Android/Java)
